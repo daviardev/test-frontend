db2c7e0a5c40cb1c883443a901b055c8edf184c6
daviardev <jerson.silva99@hotmail.com>
2024-10-28 18:13:04 -0500

    Create modal for components view

--- components/Layout/Modal.jsx
+++ components/Layout/Modal.jsx
@@ -1,6 +1,6 @@
 export default function Modal ({ children }) {
   return (
-    <div className='max-w-4xl mx-auto bg-white shadow-xl my-6 rounded-lg p-6'>
+    <div className='max-w-4xl mx-auto bg-white shadow-xl mt-20 rounded-lg p-6'>
       {children}
     </div>
   )
