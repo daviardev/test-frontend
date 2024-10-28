dbd3a25c7e0f4f694780d71c18d527e1b735cf1f
daviardev <jerson.silva99@hotmail.com>
2024-10-28 18:10:33 -0500

    Add more funcionality input

--- components/Dashboard/Input.jsx
+++ components/Dashboard/Input.jsx
@@ -1,10 +1,12 @@
-export default function Input ({ label, type }) {
+export default function Input ({ label, type, value, onChange }) {
   return (
     <div className='flex flex-col space-y-1'>
       {label && <label className='text-sm font-semibold text-description'>{label}</label>}
       <input
         type={type}
         name={label}
+        value={value}
+        onChange={onChange}
         id={label}
         className='border border-gray-300 px-3 py-2 focus:outline-none focus:ring focus:ring-button'
       />
