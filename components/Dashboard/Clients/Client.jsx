c6fb31ac4ecd3dfca7fb8bec1efa73bf6638e8f2
daviardev <jerson.silva99@hotmail.com>
2024-10-28 20:50:19 -0500

    Add Delete and fix

--- components/Dashboard/Clients/Client.jsx
+++ components/Dashboard/Clients/Client.jsx
@@ -1,16 +1,16 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 import axios from 'axios'
 
 import { FaRegEdit, FaTrash } from 'react-icons/fa'
 
-import Image from 'next/image'
-
 import Modal from '@/components/Layout/Modal'
 import Details from '@/components/Reusable/Details'
 import Input from '../Input'
+
 import toast from 'react-hot-toast'
 
 export default function Client () {
@@ -28,48 +28,42 @@ export default function Client () {
 
   const [currentClient, setCurrentClient] = useState(null)
 
-  const fetchData = async (pages = 1, limit = 1000) => {
-    const res = await axios.get('http://localhost:8000/clientes')
-    setClients(res.data)
+  const fetchData = async (page = 1, limit = 20) => {
+    try {
+      const res = await axios.get(`http://localhost:8000/clientes?_page=${page}&_limit=${limit}&_sort=id&_order=desc`)
+      setClients(res.data)
+    } catch (error) {
+      toast.error('Error fetching clients:', error)
+    }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-
     try {
+      const newClient = {
+        RUT: rut,
+        telefono: phone,
+        nombre: name,
+        apellido: lastName,
+        direccion: address
+      }
+
       if (editing && currentClient) {
-        // Actualiza el cliente
-        await axios.put(`http://localhost:8000/clientes/${currentClient.id}`, {
-          RUT: rut,
-          telefono: phone,
-          nombre: name,
-          apellido: lastName,
-          direccion: address
-        })
+        await axios.put(`http://localhost:8000/clientes/${currentClient.id}`, newClient)
         toast.success('Client updated')
       } else {
-        // Agrega un nuevo cliente
-        await axios.post('http://localhost:8000/clientes', {
-          RUT: rut,
-          telefono: phone,
-          nombre: name,
-          apellido: lastName,
-          direccion: address
-        })
+        await axios.post('http://localhost:8000/clientes', newClient)
         toast.success('Client added')
       }
 
-      // Limpia los campos después de la operación
       setRut('')
       setPhone('')
       setName('')
       setLastName('')
       setAddress('')
       setEditing(false)
-      setOpen(false)
-
-      // Actualiza la lista de clientes
-      fetchData() // Asegúrate de que esto esté después de los cambios en el cliente
+      setOpen(true)
+      fetchData()
     } catch (error) {
       console.error('Error saving client:', error)
       toast.error('Error saving client')
@@ -87,9 +81,45 @@ export default function Client () {
     setOpen(false)
   }
 
+  const handleDelete = async clientId => {
+    try {
+      await axios.delete(`http://localhost:8000/clientes/${clientId}`)
+      toast.success('Client deleted')
+      fetchData()
+    } catch (error) {
+      toast.error('Error deleting client', error)
+    }
+  }
+
   useEffect(() => {
     fetchData()
   }, [])
+
+  const renderClients = () => {
+    return clients.map((client) => (
+      <tr key={client.id} className='font-medium text-gray-800'>
+        <td className='px-6 py-3'>{client.RUT}</td>
+        <td className='px-6 py-3'>{client.nombre}</td>
+        <td className='px-6 py-3'>{client.apellido}</td>
+        <td className='px-6 py-3'>{client.direccion}</td>
+        <td className='px-6 py-3'>{client.telefono}</td>
+        <td className='flex space-x-4 items-center justify-center my-12 text-xl'>
+          <button
+            onClick={() => handleEdit(client)}
+            className='text-blue-500 rounded-full p-3 shadow-xl hover:bg-blue-100'
+          >
+            <FaRegEdit />
+          </button>
+          <button
+            onClick={() => handleDelete(client.id)}
+            className='text-red-500 rounded-full p-3 shadow-xl hover:bg-red-100'
+          >
+            <FaTrash />
+          </button>
+        </td>
+      </tr>
+    ))
+  }
 
   return (
     <Modal>
@@ -106,125 +136,61 @@ export default function Client () {
 
       <hr className='mb-8 mx-11 border-[1.8px] border-[#ceddedff]' />
 
-      {clients && (
-        <div>
-          {editing
-            ? (
-              <form onSubmit={handleSubmit}>
-                <div className='grid grid-cols-1 sm:grid-cols-12 gap-4'>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      onChange={e => setRut(e.target.value)}
-                      value={rut}
-                      label='RUT'
-                      type='text'
-                      className='w-full'
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={phone}
-                      label='Phone'
-                      type='number'
-                      className='w-full'
-                      onChange={e => setPhone(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={name}
-                      label='First Name'
-                      className='w-full'
-                      onChange={e => setName(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={lastName}
-                      label='Last Name'
-                      className='w-full'
-                      onChange={e => setLastName(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-12'>
-                    <Input
-                      value={address}
-                      label='Address'
-                      className='w-full'
-                      onChange={e => setAddress(e.target.value)}
-                    />
-                  </div>
-                </div>
-
-                <section className='flex justify-between items-center space-x-4 mt-6'>
-                  <button type='submit' className='bg-button text-white px-6 py-2 w-full sm:w-auto'>
-                    Update
-                  </button>
-                </section>
-              </form>
-              )
-            : (
-              <form onSubmit={handleSubmit}>
-                <div className='grid grid-cols-1 sm:grid-cols-12 gap-4'>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      onChange={e => setRut(e.target.value)}
-                      value={rut}
-                      label='RUT'
-                      type='text'
-                      className='w-full'
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={phone}
-                      label='Phone'
-                      type='number'
-                      className='w-full'
-                      onChange={e => setPhone(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={name}
-                      label='First Name'
-                      className='w-full'
-                      onChange={e => setName(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-6'>
-                    <Input
-                      value={lastName}
-                      label='Last Name'
-                      className='w-full'
-                      onChange={e => setLastName(e.target.value)}
-                    />
-                  </div>
-                  <div className='sm:col-span-12'>
-                    <Input
-                      value={address}
-                      label='Address'
-                      className='w-full'
-                      onChange={e => setAddress(e.target.value)}
-                    />
-                  </div>
-                </div>
-
-                <section className='flex justify-between items-center space-x-4 mt-6'>
-                  <button type='submit' className='bg-button text-white px-6 py-2 w-full sm:w-auto'>
-                    {editing ? 'Update' : 'Save'}
-                  </button>
-                  <button onClick={() => setOpen(true)} type='button' className='bg-button text-white px-6 py-2 w-full sm:w-auto'>Details</button>
-                </section>
-              </form>
-              )}
+      <form onSubmit={handleSubmit}>
+        <div className='grid grid-cols-1 sm:grid-cols-12 gap-4'>
+          <div className='sm:col-span-6'>
+            <Input
+              onChange={(e) => setRut(e.target.value)}
+              value={rut}
+              label='RUT'
+              type='text'
+              className='w-full'
+            />
+          </div>
+          <div className='sm:col-span-6'>
+            <Input
+              value={phone}
+              label='Phone'
+              type='number'
+              className='w-full'
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div className='sm:col-span-6'>
+            <Input
+              value={name}
+              label='First Name'
+              className='w-full'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='sm:col-span-6'>
+            <Input
+              value={lastName}
+              label='Last Name'
+              className='w-full'
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className='sm:col-span-12'>
+            <Input
+              value={address}
+              label='Address'
+              className='w-full'
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
         </div>
-      )}
 
-      <Details
-        open={open}
-        onClose={() => setOpen(false)}
-      >
+        <section className='flex justify-between items-center space-x-4 mt-6'>
+          <button type='submit' className='bg-button text-white px-6 py-2 w-full sm:w-auto'>
+            {editing ? 'Update' : 'Save'}
+          </button>
+          <button onClick={() => setOpen(true)} type='button' className='bg-button text-white px-6 py-2 w-full sm:w-auto'>Details</button>
+        </section>
+      </form>
+
+      <Details open={open} onClose={() => setOpen(false)}>
         <div className='w-screen max-w-5xl'>
           <div className='mx-auto my-4'>
             <h3 className='font-bold text-title'>Filter clients</h3>
@@ -245,7 +211,7 @@ export default function Client () {
                     <div className='text-left font-semibold'>Last name</div>
                   </th>
                   <th className='p-2'>
-                    <div className='text-left-font-semibold'>Address</div>
+                    <div className='text-left font-semibold'>Address</div>
                   </th>
                   <th className='p-2'>
                     <div className='text-center font-semibold'>Phone</div>
@@ -257,33 +223,12 @@ export default function Client () {
               </thead>
 
               <tbody className='divide-y divide-gray-100 text-sm'>
-                {clients.slice(0).reverse().map(client => (
-                  <tr key={client.RUT} className='font-medium text-gray-800'>
-                    <td className='px-6 py-3 text-gray-800'>{client.RUT}</td>
-                    <td className='px-6 py-3 text-gray-800'>{client.nombre}</td>
-                    <td className='px-6 py-3 text-gray-800'>{client.apellido}</td>
-                    <td className='px-6 py-3 text-gray-800'>{client.direccion}</td>
-                    <td className='px-6 py-3 text-gray-800'>{client.telefono}</td>
-                    <td className='flex space-x-4 items-center justify-center my-12 text-xl'>
-                      <button
-                        onClick={() => handleEdit(client)}
-                        className='text-blue-500 rounded-full p-3 shadow-xl hover:bg-blue-100'
-                      >
-                        <FaRegEdit />
-                      </button>
-                      <button className='text-red-500 rounded-full p-3 shadow-xl hover:bg-red-100'>
-                        <FaTrash />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
+                {renderClients()}
               </tbody>
             </table>
           </div>
         </div>
       </Details>
-
     </Modal>
-
   )
 }
